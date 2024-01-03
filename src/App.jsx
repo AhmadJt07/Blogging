9e31f15cda5ad8e9b3724a4e500e5446fce6376a
Ahmad <ahmad03065917176@gmail.com>
2024-01-03 14:42:32 +0500

    Correct nopage file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 import Home from "./pages/home/Home";
 import Blog from "./pages/blog/Blog";
 import AllBlogs from "./pages/allBlogs/AllBlogs";
-import NoPage from "./pages/nopage/NoPage";
+import NoPage from "./pages/nopage/Nopage";
 import BlogInfo from "./pages/blogInfo/BlogInfo";
 import AdminLogin from "./pages/admin/adminLogin/AdminLogin";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
@@ -25,32 +25,37 @@ function App() {
           <Route path="/allblogs" element={<AllBlogs />} />
           <Route path="/bloginfo/:id" element={<BlogInfo />} />
           <Route path="/adminlogin" element={<AdminLogin />} />
-          <Route path="/dashboard" element={
-            <ProtectedRouteForAdmin>
-              <Dashboard />
-            </ProtectedRouteForAdmin>
-          } />
-          <Route path="/createblog" element={
-            <ProtectedRouteForAdmin>
-              <CreateBlog />
-            </ProtectedRouteForAdmin>
-          } />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRouteForAdmin>
+                <Dashboard />
+              </ProtectedRouteForAdmin>
+            }
+          />
+          <Route
+            path="/createblog"
+            element={
+              <ProtectedRouteForAdmin>
+                <CreateBlog />
+              </ProtectedRouteForAdmin>
+            }
+          />
           <Route path="/*" element={<NoPage />} />
         </Routes>
         <Toaster />
       </Router>
     </MyState>
-  )
+  );
 }
 
-export default App
+export default App;
 
 export const ProtectedRouteForAdmin = ({ children }) => {
-  const admin = JSON.parse(localStorage.getItem('admin'))
+  const admin = JSON.parse(localStorage.getItem("admin"));
   if (admin?.user?.email === "runtygenious@gmail.com") {
-    return children
+    return children;
+  } else {
+    return <Navigate to={"/adminlogin"} />;
   }
-  else {
-    return <Navigate to={'/adminlogin'} />
-  }
-}
+};
